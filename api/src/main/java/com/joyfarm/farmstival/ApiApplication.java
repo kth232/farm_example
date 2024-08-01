21ef34e4204d14ac94a66aaa11ea3b5e747ed432
kth232 <kth7537@naver.com>
2024-08-01 20:24:35 +0900

    server initial setting

--- api/src/main/java/com/joyfarm/farmstival/ApiApplication.java
+++ api/src/main/java/com/joyfarm/farmstival/ApiApplication.java
@@ -3,12 +3,11 @@ package com.joyfarm.farmstival;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@RefreshScope //@RefreshScope는 설정 정보가 바뀌면 다시 불러올 수 있도록 도와줌
 @SpringBootApplication
-public class FarmstivalApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FarmstivalApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
