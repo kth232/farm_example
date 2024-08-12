ea459dc334f7ee2d4fee05b23366322c97e153d4
kth232 <kth7537@naver.com>
2024-08-12 11:17:36 +0900

    게시판 추가, 시큐리티 수정

--- admin/src/main/java/com/joyfarm/member/controllers/MemberController.java
+++ admin/src/main/java/com/joyfarm/member/controllers/MemberController.java
@@ -1,8 +1,8 @@
 package com.joyfarm.member.controllers;
 
 
-import com.joyfarm.global.Utils;
 import com.joyfarm.global.exceptions.ExceptionProcessor;
+import com.joyfarm.member.MemberUtil;
 import com.joyfarm.member.services.MemberSaveService;
 import com.joyfarm.member.validators.JoinValidator;
 import jakarta.validation.Valid;
@@ -14,10 +14,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 @Slf4j
 @Controller
 @RequestMapping("/member")
@@ -27,7 +23,7 @@ public class MemberController implements ExceptionProcessor {
 
     private final JoinValidator joinValidator;
     private final MemberSaveService memberSaveService;
-    private final Utils utils;
+    private final MemberUtil memberUtil;
 
     @ModelAttribute
     public RequestLogin requestLogin() {
@@ -36,31 +32,27 @@ public class MemberController implements ExceptionProcessor {
 
     @GetMapping("/join")
     public String join(@ModelAttribute RequestJoin form, Model model) {
-        commonProcess("join", model);
+        model.addAttribute("addCss", "join");
 
-
-        return utils.tpl("member/join");
+        return "member/join";
     }
 
     @PostMapping("/join")
     public String joinPs(@Valid RequestJoin form, Errors errors, Model model) {
-        commonProcess("join", model);
-
+//        memberSaveService.save(form);
+        model.addAttribute("addCss", "join");
         joinValidator.validate(form, errors);
-
+        errors.getAllErrors().forEach(System.out::println);
         if (errors.hasErrors()) {
-            return "front/member/join";
+            return "member/join";
         }
-
         memberSaveService.save(form);
-
         return "redirect:/member/login";
     }
 
     @GetMapping("/login")
     public String login(@Valid @ModelAttribute RequestLogin form, Errors errors, Model model) {
-        commonProcess("login", model);
-
+        model.addAttribute("addCss", "join");
         String code = form.getCode();
         if (StringUtils.hasText(code)) {
             errors.reject(code, form.getDefaultMessage());
@@ -69,34 +61,6 @@ public class MemberController implements ExceptionProcessor {
                 return "redirect:/member/password/reset ";
             }
         }
-        return utils.tpl("member/login");
-    }
-
-    /**
-     * 회원 관련 컨트롤러 공통 처리
-     *
-     * @param mode
-     * @param model
-     */
-    private void commonProcess(String mode, Model model) {
-        mode = Objects.requireNonNullElse(mode, "join");
-
-        List<String> addCss = new ArrayList<>();
-        List<String> addCommonScript = new ArrayList<>();
-        List<String> addScript = new ArrayList<>();
-
-        addCss.add("member/style");  // 회원 공통 스타일
-        if (mode.equals("join")) {
-            addCommonScript.add("fileManager");
-            addCss.add("member/join");
-            addScript.add("member/join");
-
-        } else if (mode.equals("login")) {
-            addCss.add("member/login");
-        }
-
-        model.addAttribute("addCss", addCss);
-        model.addAttribute("addCommonScript", addCommonScript);
-        model.addAttribute("addScript", addScript);
+        return "member/login";
     }
 }
