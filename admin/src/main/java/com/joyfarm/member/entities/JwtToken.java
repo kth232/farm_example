0055c88e91565cfdde2e51eeeaf1230415b53d55
kth232 <kth7537@naver.com>
2024-08-12 11:50:44 +0900

    게시판 추가, 시큐리티 수정

--- admin/src/main/java/com/joyfarm/member/entities/JwtToken.java
+++ admin/src/main/java/com/joyfarm/member/entities/JwtToken.java
@@ -7,8 +7,9 @@ import org.springframework.data.redis.core.RedisHash;
 import java.io.Serializable;
 
 @Data
-@RedisHash(timeToLive = 3600L)
+@RedisHash(timeToLive = 3600L) //1시간 유효
 public class JwtToken implements Serializable {
+
     @Id
     private String sessionId; // 세션 ID
 
