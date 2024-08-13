1ea91e123c8a8918935f15f9855334df5eec0436
kth232 <kth7537@naver.com>
2024-08-13 19:32:10 +0900

    리스트 조회 구현 중

--- api/src/main/java/com/joyfarm/global/configs/BeanConfig.java
+++ api/src/main/java/com/joyfarm/global/configs/BeanConfig.java
@@ -2,12 +2,24 @@ package com.joyfarm.global.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanConfig {
+
+    private final EntityManager em;
+
+    @Bean //엔티티 객체 조립
+    public JPAQueryFactory jpaQueryFactory() {
+
+        return new JPAQueryFactory(em);
+    }
     //자주 사용하는 2가지 수동 등록 빈으로 등록
     @Bean
     public ObjectMapper objectMapper() {
